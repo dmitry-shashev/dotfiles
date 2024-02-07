591e0747b729eb936ff1ac83859d666c33d47abe
Dmitry <old.reaver@gmail.com>
2024-02-07 20:32:07 +0200

    Improve telescope

--- files/linux/.config/nvim/lua/config/plugins/telescope.lua
+++ files/linux/.config/nvim/lua/config/plugins/telescope.lua
@@ -3,9 +3,36 @@ return {
   tag = '0.1.5',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    function vim.getVisualSelection()
+      vim.cmd('noau normal! "vy"')
+      local text = vim.fn.getreg('v')
+      vim.fn.setreg('v', {})
+
+      text = string.gsub(text, "\n", "")
+      if #text > 0 then
+        return text
+      else
+        return ''
+      end
+    end
+
+    local opts = {noremap = true, silent = true}
+
+    -- for visual mode we try to find the selected
+    local tb = require('telescope.builtin')
+    local keymap = vim.keymap.set
+    keymap ('v', ';f', function()
+      local text = vim.getVisualSelection()
+      tb.find_files({ default_text = text })
+    end, opts)
+    keymap('v', ';s', function()
+      local text = vim.getVisualSelection()
+      tb.live_grep({ default_text = text })
+    end, opts)
+
+
     local map = vim.api.nvim_set_keymap
-    local default_opts = {noremap = true, silent = true}
-    map('n', ';f', ':<C-u>Telescope find_files<CR>', default_opts)
-    map('n', ';s', ':<C-u>Telescope live_grep<CR>', default_opts)
+    map('n', ';f', ':<C-u>Telescope find_files<CR>', opts)
+    map('n', ';s', ':<C-u>Telescope live_grep<CR>', opts)
   end
 }
