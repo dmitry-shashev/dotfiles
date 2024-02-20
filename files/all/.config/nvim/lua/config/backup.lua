bd3396570649cdb71e76a8cd0f8b78026f13f3a8
Dmitry <old.reaver@gmail.com>
2024-02-20 13:25:18 +0200

    Update backup interval

--- files/all/.config/nvim/lua/config/backup.lua
+++ files/all/.config/nvim/lua/config/backup.lua
@@ -1,5 +1,6 @@
 function updateBackup()
-  vim.fn.timer_start(5000, function()
+  -- 5 min interval
+  vim.fn.timer_start(300000, function()
     if not (io.open("./.gitignore", "r") == nil) then
       local command = 'silent !node ~/scripts/zsh/extra/backup.js ' .. vim.fn.getcwd()
       vim.cmd(command)
