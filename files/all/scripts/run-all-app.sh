f06358c7e2391e92c486d76f59ceb2261bca1b8c
Dmitry <old.reaver@gmail.com>
2024-09-13 11:23:31 +0300

    Run all

--- files/all/scripts/run-all-app.sh
+++ files/all/scripts/run-all-app.sh
@@ -4,29 +4,35 @@ i3-msg 'workspace 1'
 sleep 1
 i3-msg 'exec google-chrome-stable'
 sleep 4
-i3-msg 'workspace 2'
-sleep 1
-i3-msg 'exec webstorm'
-sleep 4
+
+# i3-msg 'workspace 2'
+# sleep 1
+# i3-msg 'exec webstorm'
+# sleep 4
+#
 i3-msg 'workspace 3'
 sleep 1
 i3-msg 'exec kitty tmux a -t console'
 sleep 4
+
 i3-msg 'workspace 5'
 sleep 1
 i3-msg 'exec kitty tmux a -t home'
-sleep 2
+sleep 4
+
 i3-msg 'workspace 8'
 sleep 1
 i3-msg 'exec thunderbird'
-sleep 2
+sleep 3
+
 i3-msg 'workspace 9'
 sleep 1
 i3-msg 'exec telegram-desktop'
-sleep 2
-i3-msg 'workspace 10'
-sleep 1
-i3-msg 'exec slack'
+# sleep 2
+
+# i3-msg 'workspace 10'
+# sleep 1
+# i3-msg 'exec slack'
 
 # instead "nvidia-settings"
 xrandr --output HDMI-0 --mode 3840x2160 --rate 144
