8072e1799af2d402210a5020f5ef2767220b1940
Dmitry <old.reaver@gmail.com>
2025-09-16 10:51:01 +0000

    Fix lang toggle for laptop

--- files/all/scripts/laptop/lang-toggle.sh
+++ files/all/scripts/laptop/lang-toggle.sh
@@ -5,12 +5,9 @@ CURRENT_LAYOUT=$(setxkbmap -query | grep layout | awk '{print $2}')
 
 # Toggle between layouts
 if [ "$CURRENT_LAYOUT" == "ru" ]; then
-    # new ubuntu version will reset layout to US if we do it vice versa
-    ~/scripts/keyboard-asus.sh
-    setxkbmap us
+    setxkbmap us -option caps:escape
     notify-send "US" -t 400
 else
-    ~/scripts/keyboard-asus.sh
-    setxkbmap ru
+    setxkbmap ru -option caps:escape
     notify-send "RU" -t 400
 fi
