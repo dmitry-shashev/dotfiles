6dd0b6494be5c40c06b8de22b5e24f0519e03845
Dmitry <dshashev@shutterstock.com>
2026-07-13 18:25:53 +0100

    So on

--- files/all/.config/nvim/lua/config/plugins/treesitter.lua
+++ files/all/.config/nvim/lua/config/plugins/treesitter.lua
@@ -1,30 +1,32 @@
 -- it adds highlights
+local parsers = {
+  "bash",
+  "css",
+  "gitignore",
+  "graphql",
+  "html",
+  "http",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "scss",
+  "sql",
+  "tsx",
+  "typescript",
+  "yaml",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
+  build = function()
+    require("nvim-treesitter").install(parsers):wait(300000)
+  end,
   config = function()
-    local parsers = {
-      "bash",
-      "css",
-      "gitignore",
-      "graphql",
-      "html",
-      "http",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "scss",
-      "sql",
-      "tsx",
-      "typescript",
-      "yaml",
-    }
-
     require("nvim-treesitter").setup({})
-    require("nvim-treesitter").install(parsers)
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = {
